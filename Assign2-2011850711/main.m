c6c15a28293765df001ab3dd11e3854517d8b763
Max Kan [Pukunui] <max@pukunui.com>
2012-11-11 18:36:04 +0800

    Initial Commit

---
+++ Assign2-2011850711/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Assign2-2011850711
+//
+//  Created by Max Kan on 11/11/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
